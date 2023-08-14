97319d737445abd3fe58bc4db577b4e82b947261
SARB <mrsinghbusiness05@gmail.com>
2023-08-14 17:31:44 +0530

    accordian structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import Counter from './Components/Counter'
+import Accordian from './Components/Accordian'
 
 function App() {
-
   return (
     <>
         <div className = "mainapp-container" style = {{textAlign:'center'}} >
            <h2> Counter App  </h2>
-           <Counter />
+            <Accordian />
         </div>
     </>
   )
