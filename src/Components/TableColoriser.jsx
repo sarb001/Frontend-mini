8ab772160e1b9551f59e66719858af25a3d5c7ad
SARB <mrsinghbusiness05@gmail.com>
2023-08-15 13:26:30 +0530

    tablecolorizer working

--- src/Components/TableColoriser.jsx
+++ src/Components/TableColoriser.jsx
@@ -1,33 +1,39 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './TableColoriser.css';
+const  length = 9;   
+const numberList = Array.from(new Array(length) , (_,i) => i);
 
 const TableColoriser = () => {
+
+     const [colorArray,setcolorArray] = useState([]);
+     const colRef = useRef();
+
+     const oncolorme = (e) => {
+        e.preventDefault();
+        if(colorArray.indexOf(parseInt(colRef.current.value)) === -1)
+        {
+            setcolorArray((prev) => [...prev ,parseInt(colRef.current.value)]);
+        }
+     }
+
   return (
     <>
           <h2> Table Colorizer  </h2>
-          <div> <input type = "number"  /> </div>
-          <div style = {{margin:'1%'}}>  <button> Color me </button>
-                 <button> Clear me  </button>
-          </div>
-        <div className="table-colorizer" style = {{display:'flex',justifyContent:'center',cursor:'pointer'}}>
-            <table>
-                <tr>
-                    <td> 1 </td>
-                    <td> 2 </td>
-                    <td> 3 </td>
-                </tr>
-                <tr>
-                    <td> 4 </td>
-                    <td> 5 </td>
-                    <td> 6 </td>
-                </tr>
-                <tr>
-                    <td> 7 </td>
-                    <td> 8 </td>
-                    <td> 9 </td>
-                </tr>
-            </table>
-        </div>
+                <div> <input type = "number"  className='numberInput' ref = {colRef} /> </div>
+                <div style = {{margin:'1%'}}>  
+                   <input  type = "submit" value='Color Me' onClick={oncolorme} />
+                        <button> Clear me  </button>
+                </div>
+               
+         <div className='containers'>
+                {numberList.map((e,p) => {
+                    return (
+                        <div  className={colorArray.indexOf(e + 1) !== -1 ? 'containerColor' : 'container'}>
+                         {e+1}
+                        </div>
+                    )
+                    })}
+           </div>
     </>
   )
 }
