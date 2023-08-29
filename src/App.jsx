c550a1a355204282d55a0b47152f6c38be2d8936
SARB <mrsinghbusiness05@gmail.com>
2023-08-29 10:43:39 +0530

    mutiple-accordian-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react';
+import questions from './Components/Questions';
+
+import '../src/styles/App.css';
 import Accordian from './Components/Accordian';
 
 function App() {
+
   return (
     <>
         <div className = "mainapp-container" style = {{textAlign:'center',fontSize:'25px'}} >
-          <Accordian />
+          
+             {questions.map((item) => {
+              return (
+                  <Accordian  key = {item.id} title = {item.title} content = {item.info} />
+               )
+             })}          
+
         </div>
     </>
   )
--- src/Components/Accordian.jsx
+++ src/Components/Accordian.jsx
@@ -1,62 +1,22 @@
 import React, { useState } from 'react'
-import '../styles/App.css';
 
-const Accordian = () => {
+const Accordian = ({title,content}) => {
 
-   const [show,setshow] = useState(false);
+   const [isExpanded,setisExpanded] = useState(false);
 
    const showdata = () => {
-       setshow((prev) => !prev);
+     setisExpanded(prev => !prev);
    }
 
   return (
     <>
-        <h3> Accordian React </h3>
-       <div className="accordian-container">
+          <div className="accordion-item">
+            <button className="accordion-header" onClick={showdata}>
+              {title}
+            </button>
+            {isExpanded && <div className="accordion-content"> {content} </div>}
+          </div> 
 
-            <div className="accordianitem">
-               <div className="accordianheader">
-                 <span> Do i have to Allow First Part  1 </span>
-                 <span className='symbol'  onClick={showdata} > {show ? '-' : '+' }  </span>
-               </div>
-               <div className= {`accordian-detail ${show ? 'showdata' : 'hidedata' }`}>
-                 <p> 111 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                   Ex fuga quisquam deleniti commodi praesentium saepe ipsa repellat 
-                   incidunt nemo quos harum iste mollitia fugit eum esse eveniet necessitatibus nam,
-                    quis iure. Quae quaerat fuga provident. 
-                  </p>
-               </div>
-            </div>
-
-            <div className="accordianitem">
-               <div className="accordianheader">
-                 <span> Do i have to Allow First Part  2 </span>
-                 <span className='symbol'  onClick={showdata} > {show ? '-' : '+' }  </span>
-               </div>
-               <div className= {`accordian-detail ${show ? 'showdata' : 'hidedata' }`}>
-                 <p> 22222 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                   Ex fuga quisquam deleniti commodi praesentium saepe ipsa repellat 
-                   incidunt nemo quos harum iste mollitia fugit eum esse eveniet necessitatibus nam,
-                    quis iure. Quae quaerat fuga provident. 
-                  </p>
-               </div>
-            </div>
-
-            <div className="accordianitem">
-               <div className="accordianheader">
-                 <span> Do i have to Allow First Part  3 </span>
-                 <span className='symbol'  onClick={showdata} > {show ? '-' : '+' }  </span>
-               </div>
-               <div className= {`accordian-detail ${show ? 'showdata' : 'hidedata' }`}>
-                 <p> 3333 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                   Ex fuga quisquam deleniti commodi praesentium saepe ipsa repellat 
-                   incidunt nemo quos harum iste mollitia fugit eum esse eveniet necessitatibus nam,
-                    quis iure. Quae quaerat fuga provident. 
-                  </p>
-               </div>
-            </div>
-
-       </div>
     </>
   )
 }
