2d385da71d390eead5640403c5f98fc9d1a47d3c
SARB <mrsinghbusiness05@gmail.com>
2023-08-30 06:39:55 +0530

    Progress bar -react

--- src/Components/ProgressBar.jsx
+++ src/Components/ProgressBar.jsx
@@ -1,21 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css' ;
 
 const ProgressBar = () => {
+
+        const [progress, setProgress] = useState(0);
+        const [running, setRunning] = useState(false);
+
+        useEffect(() => {
+            let interval;
+            if (running) {
+              interval = setInterval(() => {
+                setProgress((prevProgress) => {
+                  if (prevProgress < 100) {
+                    return prevProgress + 1;
+                  } else {
+                    clearInterval(interval);
+                    setRunning(false);
+                    return prevProgress;
+                  }
+                });
+              }, 100);
+            } else {
+              clearInterval(interval);
+            }
+        
+            return () => {
+              clearInterval(interval);
+            };
+          }, [running]);
+
+          const handleStart = () => {
+            setRunning(true);
+          };
+        
+          const handleStop = () => {
+            setRunning(false);
+          };
+        
+          const handleReset = () => {
+            setRunning(false);
+            setProgress(0);
+          };
+
   return (
-        <div className="progress-container">
-             <progress id = "bar" value="5" min = "1" max = "100" >  </progress>
-             <div className = "buttons" >
-                <div>  <button id = "start-btn"> Start </button> </div>
-                <div>  <button id = "pause-btn"> Pause  </button> </div>
-                <div>  <button id = "reset-btn"> Reset </button></div>
-               
-             </div>
-             <div className="rangebar">
-                 <label> Speed : 1 </label>
-                <input type  = "range" value= "1"  min = "1" max = "10" />
-             </div>
-        </div>
+        <>
+          <div>
+      <div className="progress-container">
+            <div className="progress" style={{ width: `${progress}%` }}>
+            {progress}%
+            </div>
+      </div>
+
+      <div className="button-container">
+            <button onClick={handleStart} disabled={running}>
+            Start
+            </button>
+
+            <button onClick={handleStop} disabled={!running}>
+            Stop
+            </button>
+
+            <button onClick={handleReset} disabled={running}>
+            Reset
+            </button>
+      </div>
+    </div>
+                  
+                   
+                  
+                   
+        </>
   )
 }
 
